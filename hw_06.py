85389d615bc92a1857ba9e5df74b0f9cd2df966c
Микола Винничук <62299632+Mykola-Vynnychuk@users.noreply.github.com>
2023-06-09 14:29:14 -0700

    Create sort_folder

--- hw_06.py
+++ hw_06.py
@@ -1,6 +1,14 @@
 import sys
 from pathlib import Path
 
+
+CATEGORIES = {"Audio": ["mp3", "aiff"],
+              "Documents": ["docx", "txt", "pdf"]}
+
+def sort_folder(path: Path) -> None:
+    for item in path.glob("**/*"):
+        print (item)
+
 def main():
     try:
         path = Path(sys.argv[1])
@@ -10,6 +18,7 @@ def main():
     if not path.exists():
         return f"Folder with path {path} dos not exists."
     
+    sort_folder(path)
     return "All ok"
 
 if __name__ == "__main__":
