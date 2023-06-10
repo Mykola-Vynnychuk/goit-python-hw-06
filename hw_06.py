b3b3e23e8e78272dc7e28858f5b3a72441fbf1a0
Микола Винничук <62299632+Mykola-Vynnychuk@users.noreply.github.com>
2023-06-09 23:45:46 -0700

    Add move_files

--- hw_06.py
+++ hw_06.py
@@ -5,6 +5,13 @@ from pathlib import Path
 CATEGORIES = {"Audio": [".mp3", ".aiff"],
               "Documents": [".docx", ".txt", ".pdf"]}
 
+def move_file(path: Path, root_dir: Path, categorie: str) -> None:
+    target_dir = root_dir.joinpath(categorie)
+    if not target_dir.exists():
+        print(f"Make {target_dir}")
+        target_dir.mkdir()
+    print(f"Exist {target_dir}")
+
 def get_categories(path: Path) -> str:
     ext = path.suffix
     for cat, exts in CATEGORIES.items():
@@ -15,7 +22,8 @@ def get_categories(path: Path) -> str:
 def sort_folder(path: Path) -> None:
     for item in path.glob("**/*"):
         if item.is_file:
-            print(get_categories(item))
+            cat = get_categories(item)
+            move_file(item, path, cat)
 
 def main():
     try:
