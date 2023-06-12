1f99b0eb84481f22c9fe9b877d1fb8fd2b8dbf90
Микола Винничук <62299632+Mykola-Vynnychuk@users.noreply.github.com>
2023-06-11 22:26:35 -0700

    Add normalize

--- hw_06.py
+++ hw_06.py
@@ -5,12 +5,33 @@ from pathlib import Path
 CATEGORIES = {"Audio": [".mp3", ".aiff"],
               "Documents": [".docx", ".txt", ".pdf"]}
 
+CYRILLIC_SYMBOLS = "абвгґдеёєжзиіїйклмнопрстуфхцчшщъыьэюя"
+
+TRANSLATION = ("a", "b", "v", "h", "g", "d", "e", "e", "ie", "zh", 
+               "z", "y", "i", "i", "i", "k", "l", "m", "n", "o", "p", 
+               "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", 
+               "", "y", "", "e", "iu", "ia")
+
+BAD_SYMBOLS = ("%", "*", " ", "-")
+
+TRANS = {}
+
+for c, t in zip(list(CYRILLIC_SYMBOLS), TRANSLATION):
+    TRANS[ord(c)] = t
+    TRANS[ord(c.upper())] = t.upper()
+
+for i in BAD_SYMBOLS:
+    TRANS[ord(i)] = "_"
+
+def normalize(name: str) -> str:
+    return name.translate(TRANS)
+    
 def move_file(path: Path, root_dir: Path, categorie: str) -> None:
     target_dir = root_dir.joinpath(categorie)
     if not target_dir.exists():
         print(f"Make {target_dir}")
         target_dir.mkdir()
-    print(f"Exist {target_dir}")
+    path.replace(target_dir.joinpath(f"{normalize(path.stem)}{path.suffix}"))
 
 def get_categories(path: Path) -> str:
     ext = path.suffix
