e76157c46edb9846cea9a0ff5e207c78a8351010
Микола Винничук <62299632+Mykola-Vynnychuk@users.noreply.github.com>
2023-06-12 18:08:27 +0300

    Add unpack_archive

--- hw_06.py
+++ hw_06.py
@@ -1,8 +1,10 @@
 import sys
 from pathlib import Path
+import uuid
+import shutil
 
 
-CATEGORIES = {"Archives": [".zip", ".gz", ".tar"],
+CATEGORIES = {"Archives": [".zip", ".gz", ".tar", ".rar"],
               "Audio": [".mp3", ".ogg", ".wav", ".amr"],
               "Documents": [".doc", ".docx", ".txt", ".pdf", ".xlsx", ".pptx"],
               "Images": [".jpeg", ".png", ".jpg", ".svg"],
@@ -33,7 +35,10 @@ def move_file(file: Path, root_dir: Path, categorie: str) -> None:
     target_dir = root_dir.joinpath(categorie)
     if not target_dir.exists():
         target_dir.mkdir()
-    file.replace(target_dir.joinpath(f"{normalize(file.stem)}{file.suffix}"))
+    new_name = target_dir.joinpath(f"{normalize(file.stem)}{file.suffix}")
+    if new_name.exists():
+        new_name = new_name.with_name(f"{new_name.stem}-{uuid.uuid4()}{file.suffix}")
+    file.rename(new_name)
 
 def get_categories(file: Path) -> str:
     ext = file.suffix
@@ -42,12 +47,22 @@ def get_categories(file: Path) -> str:
             return cat
     return "Other"
 
-def delete_empty(path: Path) -> None:
+def delete_empty_folder(path: Path) -> None:
     for item in path.iterdir():
         if item.is_dir():
-            delete_empty(item)
+            delete_empty_folder(item)
             if not any(item.iterdir()):
                 item.rmdir()
+
+def unpack_archive(path: Path) -> None:
+    archives_folder = path / "Archives"
+    for file_path in archives_folder.iterdir():
+        if file_path.is_file():
+            try:
+                shutil.unpack_archive(str(file_path), str(archives_folder / file_path.stem))
+                file_path.unlink()
+            except:
+                continue
 
 def sort_folder(path: Path) -> None:
     path_list = []
@@ -57,7 +72,7 @@ def sort_folder(path: Path) -> None:
         if i.is_file():
             move_file(i, path, get_categories(i))
         else: 
-            delete_empty(path)
+            delete_empty_folder(path)
 
 def main():
     try:
@@ -69,6 +84,7 @@ def main():
         return f"Folder with path {path} dos not exists."
     
     sort_folder(path)
+    unpack_archive(path)
     return "All ok"
 
 if __name__ == "__main__":
